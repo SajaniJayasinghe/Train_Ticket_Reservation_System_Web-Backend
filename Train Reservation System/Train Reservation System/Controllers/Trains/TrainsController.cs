9d7802c556318e51039c4f3b67a3eb8ac2a6cf4d
Randima Dilshani <randimadilshani98@gmail.com>
2023-10-16 19:22:52 +0530

    Train Responses Update

--- Train Reservation System/Train Reservation System/Controllers/Trains/TrainsController.cs
+++ Train Reservation System/Train Reservation System/Controllers/Trains/TrainsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Train_Reservation_System.Helpers;
 using Train_Reservation_System.Models.Trains;
+using Train_Reservation_System.Models.Travelers;
 using Train_Reservation_System.Services.Trains;
+using Train_Reservation_System.Services.Travelers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,89 +19,86 @@ namespace Train_Reservation_System.Controllers.Trains
         {
             this.trainService = trainService;
         }
+    
         // GET: api/<TrainsController>
         [HttpGet]
         public ActionResult<List<Train>> Get()
         {
-            return trainService.Get();
-        }
+            try
+            {
+                var trains = trainService.Get();
+                return Ok(new ApiResponse(true, 200, "Success", trains));
 
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ApiResponse(false, 500, ex.Message, null));
+            }
+        }
 
         // GET api/<TrainsController>/5
         [HttpGet("{id}")]
         public ActionResult<Train> Get(string id)
         {
-            var train = trainService.Get(id);
-            if (train == null)
+            try
             {
-                return NotFound($"Train with Id = {id} not found");
+                var train = trainService.Get(id);
+                if (train == null)
+                {
+                    return NotFound(new ApiResponse(false, 404, $"Train with ID = {id} not found", null));
+                }
+                return Ok(new ApiResponse(true, 200, "Success", train));
             }
-            return train;
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ApiResponse(false, 500, ex.Message, null));
+            }
         }
 
         // POST api/<TrainsController>
         [HttpPost]
-        public ActionResult<Train> Post([FromBody] Train train)
+        public ActionResult<ApiResponse> CreateTrain([FromBody] Train train)
         {
-            trainService.Create(train);
-            return CreatedAtAction(nameof(Get), new { id = train.Id }, train);
+            try
+            {
+                trainService.Create(train);
+                return new ApiResponse(true, 201, "Train created successfully", train);
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse(false, 500, "Error creating Train", ex.Message);
+            }
         }
 
         // PUT api/<TrainsController>/5
         [HttpPut("{id}")]
         public ActionResult Put(string id, [FromBody] Train train)
-
         {
             var existingTrain = trainService.Get(id);
 
             if (existingTrain == null)
             {
-                return NotFound($"Train with Id = {id} not found");
+                return NotFound(new ApiResponse(false, 401, $"Train with ID = {id} not found", null));
             }
+
             trainService.Update(id, train);
-            return NoContent();
+
+            return Ok(new ApiResponse(true, 200, "Train updated successfully", existingTrain));
         }
 
 
-
-        // DELETE api/<TrainsController>/5
-        [HttpDelete("{id}")]
-        public ActionResult Delete(string id)
-        //{
-        //    var train = trainService.Get(id);
-        //    if (train == null)
-        //    {
-        //        return NotFound($"Train with Id = {id} not found");
-        //    }
-
-        //    try
-        //    {
-        //        trainService.Remove(train.Id);
-        //        return Ok($"Train with Id = {id} deleted");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(ex.Message);
-        //    }
-
-        //}
-        {
-            var train = trainService.Get(id);
-            if (train == null)
+            // DELETE api/<TrainsController>/5
+            [HttpDelete("{id}")]
+            public ActionResult Delete(string id)
             {
-                return NotFound($"Train with Id = {id} not found");
-            }
-
-            try
-            {
+                var train = trainService.Get(id);
+                if (train == null)
+                {
+                    return NotFound(new ApiResponse(false, 401, $"Train with ID = {id} not found", null));
+                }
                 trainService.Remove(train.Id);
-                return Ok($"Train with Id = {id} deleted");
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
+                return Ok(new ApiResponse(true, 200, "Success", train));
             }
 
         }
     }
-}
